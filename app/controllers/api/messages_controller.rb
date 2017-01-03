6ca26f796ee2591525245738590fc84851f5b2e4
jcincotta22 <jcincotta22@gmail.com>
2017-01-03 16:20:08 -0500

    submit form

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,17 @@
 class Api::MessagesController < ApiController
   def index
     @messages = Message.all
-    render json: { messages: @messages }, status: :ok
   end
+
+  def create
+    new_message = Message.new(body: params[:message])
+    if new_message.save
+      @messages = Message.all
+      render json: { messages: @messages }, status: :ok
+    else
+      render json: { message: 'failure' }, status: :error
+    end
+
+  end
+
 end
